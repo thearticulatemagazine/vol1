2fc7f26560e6ded3ae83866db0c8562abfab0ed4
Rizwan Ishak <ibnishak@live.com>
2023-04-30 22:39:06 +0530

    [layout][(dev): MILESTONE: Remove JQuery

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,23 +1,23 @@
 function fadeIn(el, display) {
-  el.style.opacity = 0;
-  el.style.display = display || "block";
-  (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += 0.1) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  })();
+	el.style.opacity = 0;
+	el.style.display = display || "block";
+	(function fade() {
+		var val = parseFloat(el.style.opacity);
+		if (!((val += 0.1) > 1)) {
+			el.style.opacity = val;
+			requestAnimationFrame(fade);
+		}
+	})();
 }
 function fadeOut(el) {
-  el.style.opacity = 1;
-  (function fade() {
-    if ((el.style.opacity -= 0.1) < 0) {
-      el.style.display = "none";
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
+	el.style.opacity = 1;
+	(function fade() {
+		if ((el.style.opacity -= 0.1) < 0) {
+			el.style.display = "none";
+		} else {
+			requestAnimationFrame(fade);
+		}
+	})();
 }
 
 // document.onscroll = function () {
@@ -35,13 +35,22 @@ function fadeOut(el) {
 var lastScrollTop;
 navbar = document.getElementById("MagicMenu");
 window.addEventListener("scroll", function () {
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    fadeOut(navbar);
-  } else {
-    if (navbar.style.display == "none") {
-      fadeIn(navbar);
-    }
-  }
+
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	if (scrollTop > lastScrollTop) {
+		fadeOut(navbar);
+	} else {
+		if (navbar.style.display == "none") {
+			fadeIn(navbar);
+		}
+	}
+	el = document.getElementById("alertbar");
+	if (scrollTop + 700 > document.documentElement.scrollHeight - this.window.innerHeight) {
+		if (el.style.display == "none") {
+			fadeIn(el);
+		}
+	} else {
+		fadeOut(el);
+	}
   lastScrollTop = scrollTop;
 });
