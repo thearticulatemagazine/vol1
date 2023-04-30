04c4e15e746a1c28d8f97d6fb45f2c7aaf8c1804
Rizwan Ishak <ibnishak@live.com>
2023-04-30 22:25:19 +0530

    [static](dev): smooth scroll via css

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,86 +1,47 @@
-
-	$(window).scroll(function(){
-		if($(window).scrollTop() + 700 > $(document).height() - $(window).height() ){
-	  
-			$('.alertbar').fadeIn();
-	  
-		} else {
-            $('.alertbar').fadeOut();
-        }
-	  });
-
-// Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('nav').outerHeight();
-
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down            
-            $('nav').removeClass('nav-down').addClass('nav-up'); 
-            $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-           
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {               
-                $('nav').removeClass('nav-up').addClass('nav-down');
-                $('.nav-up, .nav-down').css('top', '0px');             
-            }
-        }
-
-        lastScrollTop = st;
+function fadeIn(el, display) {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
     }
-    
-    
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
-
-
-// Smooth on external page
-$(function() {
-  setTimeout(function() {
-    if (location.hash) {
-      /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
-      window.scrollTo(0, 0);
-      target = location.hash.split('#');
-      smoothScrollTo($('#'+target[1]));
+  })();
+}
+function fadeOut(el) {
+  el.style.opacity = 1;
+  (function fade() {
+    if ((el.style.opacity -= 0.1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
     }
-  }, 1);
+  })();
+}
 
-  // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-  $('a[href*=\\#]:not([href=\\#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      smoothScrollTo($(this.hash));
-      return false;
-    }
-  });
+// document.onscroll = function () {
+//   el = document.getElementById("alertbar");
+//   if ($(window).scrollTop() + 700 > $(document).height() - $(window).height()) {
+//     if (el.style.display == "none") {
+//       fadeIn(el);
+//     }
+//   } else {
+//     fadeOut(el);
+//   }
+// };
 
-  function smoothScrollTo(target) {
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
+var lastScrollTop;
+navbar = document.getElementById("MagicMenu");
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    fadeOut(navbar);
+  } else {
+    if (navbar.style.display == "none") {
+      fadeIn(navbar);
     }
   }
+  lastScrollTop = scrollTop;
 });
