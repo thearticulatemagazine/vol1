cda7e4f7709234106d9928b0939ca9693f56cb16
Rizwan Ishak M.P <madapeedikakkaran@gmail.com>
2023-07-17 12:22:35 +0530

    [MILESTONE] Move to tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
     plugins: [
 		require('autoprefixer'),
-		purgecss({
-			content: ['./**/*.html'],
-			safelist: {
-				greedy: [/^category-/],
-				standard: [/^tag-/],
-				greedy: [/^snackbar/],
-			}
-		  }),
         require('cssnano')({
             preset: 'default',
         })
